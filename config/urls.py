bbb721c7d0095097447fd35b8411a42c48ef0283
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-09-10 13:36:12 +0300

    feat(locations): add cities API endpoint

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
+    path("api/", include("apps.locations.urls")),
 ]
 
 if settings.DEBUG:
