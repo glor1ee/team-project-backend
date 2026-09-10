121960c548b2a43f60d91cda9f7cbb1ad167b0e4
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-09-10 13:33:03 +0300

    feat(locations): add City model with pickup point info

---
+++ apps/locations/models.py
@@ -0,0 +1,33 @@
+from django.core.exceptions import ValidationError
+from django.db import models
+
+
+class City(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    slug = models.SlugField(max_length=64, unique=True)
+    is_default = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    order = models.PositiveSmallIntegerField(default=0)
+
+    pickup_address = models.CharField(max_length=255, blank=True)
+    pickup_phone = models.CharField(max_length=32, blank=True)
+    working_hours = models.CharField(max_length=64, default="Пн-Нд: Цілодобово")
+
+    class Meta:
+        ordering = ["order", "name"]
+        verbose_name = "city"
+        verbose_name_plural = "cities"
+
+    def __str__(self):
+        return self.name
+
+    def clean(self):
+        if self.is_default:
+            clash = City.objects.filter(is_default=True).exclude(pk=self.pk)
+            if clash.exists():
+                raise ValidationError({"is_default": "Another city is already the default."})
+
+    def save(self, *args, **kwargs):
+        if self.is_default:
+            City.objects.filter(is_default=True).exclude(pk=self.pk).update(is_default=False)
+        super().save(*args, **kwargs)
